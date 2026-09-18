fba0c1dc4c16c91efab714fcd4ca3b0a0db4c420
Aidyn <akerembek@bk.ru>
2026-09-18 05:10:42 +0500

    HW 1 | Task 5

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,16 @@
 fun main() {
-    val numberOfAdults = "20"
-    val numberOfKids = "30"
-    val total = numberOfAdults.toInt() + numberOfKids.toInt()
-    println("The total party size is: $total")
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    var totalSalary = "$baseSalary + $bonusAmount"
+    totalSalary = calculate(baseSalary, bonusAmount)
+
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+
+//our total salary works not correctly, cuz its String, so Kotlin does not calculate sum.
+//correct way below.
+
+fun calculate(baseSalary: Int, bonusAmount: Int): String {
+    val totalSalary: Int = baseSalary + bonusAmount
+    return totalSalary.toString()
 }
