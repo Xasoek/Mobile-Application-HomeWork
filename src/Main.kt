8b97ccea99e0a40bbd5b9aac7e8548be6d0595a8
Aidyn <akerembek@bk.ru>
2026-09-18 05:46:13 +0500

    HW 1 | Task 10

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,25 @@
 fun main() {
-    var timeSpentToday = 300
-
-    var timeSpentYesterday = 250
-
-    println(compare(timeSpentToday, timeSpentYesterday))
-
-    timeSpentToday = 200
+//    println("City: Ankara")
+//    println("Low temperature: 27, High temperature: 31")
+//    println("Chance of rain: 82%")
+    println(information("Ankara", 27, 31, 82))
 
 
-    println(compare(timeSpentToday, timeSpentYesterday))
+//    println("City: Tokyo")
+//    println("Low temperature: 32, High temperature: 36")
+//    println("Chance of rain: 10%")
+    println(information("Tokyo", 32, 36, 10))
+
+//    println("City: Cape Town")
+//    println("Low temperature: 59, High temperature: 64")
+//    println("Chance of rain: 2%")
+    println(information("Cape Town", 59, 64, 2))
+
+//    println("City: Guatemala City")
+//    println("Low temperature: 50, High temperature: 55")
+//    println("Chance of rain: 7%")
+    println(information("Guatemala City", 50, 55, 7))
 }
-fun compare(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
-    return timeSpentToday > timeSpentYesterday
+fun information(city: String, lowTemp: Int, highTemp: Int, chanceOfRain: Int): String {
+    return "City: $city\nLow temperature: $lowTemp, High temperature: $highTemp\nChance of rain: $chanceOfRain\n"
 }
