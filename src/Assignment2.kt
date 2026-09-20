a7e22650e493c4780b4f9e3110baf2555ad3d72c
Aidyn <akerembek@bk.ru>
2026-09-20 23:56:25 +0500

    HW 2 | Task 7

--- src/Assignment2.kt
+++ src/Assignment2.kt
@@ -1,7 +1,5 @@
 fun main() {
-    val phone = FoldablePhone()
-
-    phone.unfold()
-    phone.switchOn()
-    phone.checkPhoneScreenLight()
+    val winningBid = Bid(5000, "Private Collector")
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
 }
