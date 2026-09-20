7d769df783f76828fa3c64aa19b20cdd77b82cdf
Aidyn <akerembek@bk.ru>
2026-09-20 23:41:39 +0500

    HW 2 | Task 5

--- src/Assignment2.kt
+++ src/Assignment2.kt
@@ -1,37 +1,7 @@
 fun main() {
-    printFinalTemperature(
-        27.0,
-        "Celsius",
-        "Fahrenheit", {celsius ->
-            9.0 / 5.0 * celsius + 32
-        })
-
-    printFinalTemperature(
-        320.0,
-        "Celsius",
-        "Fahrenheit",
-        { kelvin ->
-            kelvin - 273.15
-        }
-    )
-
-    printFinalTemperature(
-        30.0,
-        "Fahrenheit",
-        "Kelvin",
-        { fahrenheit ->
-            5.0 / 9.0 * (fahrenheit - 32) + 273.15
-        }
-    )
-}
-
-
-fun printFinalTemperature(
-    initialMeasurement: Double,
-    initialUnit: String,
-    finalUnit: String,
-    conversionFormula: (Double) -> Double
-) {
-    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
-    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+    amanda.showProfile()
+    println()
+    atiqah.showProfile()
 }
